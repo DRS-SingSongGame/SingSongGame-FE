4f8051fc2ca75bb66a3f73f65e2cb1770f21afe1
Corinbeom <105149196+Corinbeom@users.noreply.github.com>
2025-07-01 19:23:10 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  basePath: "/dev",
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
